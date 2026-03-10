6db470c183f1ab633c4389c352128fa2871bb5b7
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2026-03-10 11:00:00 +0100

    feat(db-migrations): refine changelogs and bootstrap migration service config

---
+++ src/main/java/com/nexus/db/migrations/NexusDbMigrationsApplication.java
@@ -0,0 +1,13 @@
+package com.nexus.db.migrations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexusDbMigrationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexusDbMigrationsApplication.class, args);
+	}
+
+}
